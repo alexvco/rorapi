51378e7d413a96631b21d018aa5dcdc9edf76501
Alex Varjabedian <alex@trueandco.com>
2020-09-12 13:04:49 -0700

    Serialize blog model

---
+++ app/controllers/api/v1/blogs_controller.rb
@@ -0,0 +1,55 @@
+class Api::V1::BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :update, :destroy]
+
+  # GET /blogs
+  def index
+    blogs = Blog.all
+
+    api_response(payload: BlogSerializer.new(blogs))
+  end
+
+  # GET /blogs/1
+  def show
+    api_response(payload: BlogSerializer.new(@blog))
+  end
+
+  # POST /users
+  def create
+    @blog = Blog.new(blog_params)
+
+    if @blog.save
+      api_response(payload: BlogSerializer.new(@blog), status: :created)
+    else
+      api_error(title: 'Failed to create blog', detail: @blog.errors.full_messages.to_sentence, status: :unprocessable_entity)
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @blog.update(blog_params)
+      api_response(payload: BlogSerializer.new(@blog))
+    else
+      api_error(title: 'Failed to update blog', detail: @blog.errors.full_messages.to_sentence, status: :unprocessable_entity)
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    if @blog.destroy
+      api_success(message: 'Blog was deleted successfully')
+    else
+      api_error(title: 'Failed to delete blog', detail: @blog.errors.full_messages.to_sentence, status: :unprocessable_entity)
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_blog
+      @blog = Blog.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def blog_params
+      params.require(:blog).permit(:user_id, :title, :body, :status)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :blogs
+  root 'api/v1/blogs#index'
+
   namespace 'api', defaults: { format: :json } do
     namespace 'v1' do
       resources :users
+      resources :blogs
     end
   end
 end
