b80180765503a0fb87dec939c0c62083788f964f
Alex Varjabedian <alex@trueandco.com>
2020-09-13 13:53:41 -0700

    rails g scaffold Address city zip user:references && db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :addresses
   root 'api/v1/blogs#index'
 
   namespace 'api', defaults: { format: :json } do
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200912224816) do
+ActiveRecord::Schema.define(version: 20200913205315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "city"
+    t.string "zip"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "blogs", force: :cascade do |t|
     t.bigint "user_id"
@@ -57,5 +66,6 @@ ActiveRecord::Schema.define(version: 20200912224816) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "addresses", "users"
   add_foreign_key "blogs", "users"
 end
